2139f6ceb7ce18fba9d984e04b683fb58c58ac45
hsluoyz <hsluoyz@qq.com>
2016-04-16 19:37:56 +0800

    Added the function to check operation mode.

--- WlanHelper/WlanHelper.cpp
+++ WlanHelper/WlanHelper.cpp
@@ -28,6 +28,22 @@ DWORD SetInterface(WLAN_INTF_OPCODE opcode, PVOID* pData, GUID* InterfaceGuid)
 	return dwResult;
 }
 
+DWORD GetInterface(WLAN_INTF_OPCODE opcode, PVOID* pData, GUID* InterfaceGuid)
+{
+	DWORD dwResult = 0;
+	HANDLE hClient = NULL;
+	DWORD dwCurVersion = 0;
+	DWORD outsize = 0;
+	WLAN_OPCODE_VALUE_TYPE opCode = wlan_opcode_value_type_invalid;
+
+	// Open Handle for the set operation
+	dwResult = WlanOpenHandle(WLAN_CLIENT_VERSION_VISTA, NULL, &dwCurVersion, &hClient);
+	dwResult = WlanQueryInterface(hClient, InterfaceGuid, opcode, NULL, &outsize, pData, &opCode);
+	WlanCloseHandle(hClient, NULL);
+
+	return dwResult;
+}
+
 // enumerate wireless interfaces
 UINT EnumInterface(HANDLE hClient, WLAN_INTERFACE_INFO sInfo[64])
 {
@@ -157,6 +173,30 @@ GetInterfaceStateString(__in WLAN_INTERFACE_STATE wlanInterfaceState)
 	return strRetCode;
 }
 
+// get interface operation mode string
+LPWSTR
+GetInterfaceOperationModeString(__in ULONG wlanInterfaceOperationMode)
+{
+	LPWSTR strRetCode;
+
+	switch (wlanInterfaceOperationMode)
+	{
+	case DOT11_OPERATION_MODE_EXTENSIBLE_STATION:
+		strRetCode = L"\"Extensible Station (ExtSTA)\"";
+		break;
+	case DOT11_OPERATION_MODE_NETWORK_MONITOR:
+		strRetCode = L"\"Network Monitor (NetMon)\"";
+		break;
+	case DOT11_OPERATION_MODE_EXTENSIBLE_AP:
+		strRetCode = L"\"Extensible Access Point (ExtAP)\"";
+		break;
+	default:
+		strRetCode = L"\"invalid interface operation mode\"";
+	}
+
+	return strRetCode;
+}
+
 int main()
 {
 	HANDLE hClient = NULL;
@@ -173,11 +213,26 @@ int main()
 	{
 		if (UuidToStringA(&sInfo[i].InterfaceGuid, &strGuid) == RPC_S_OK)
 		{
-			printf(("%d. %s\n\tDescription: %S\n\tState: %S\n"),
+			ULONG OperationMode = -1;
+			PULONG pOperationMode;
+			DWORD dwResult = GetInterface(wlan_intf_opcode_current_operation_mode, (PVOID*)&pOperationMode, &sInfo[i].InterfaceGuid);
+			if (dwResult != ERROR_SUCCESS)
+			{
+				printf("GetInterface error, error code = %d\n", dwResult);
+				system("PAUSE");
+			}
+			else
+			{
+				OperationMode = *pOperationMode;
+				WlanFreeMemory(pOperationMode);
+			}
+
+			printf(("%d. %s\n\tDescription: %S\n\tState: %S\n\tOperation Mode: %S\n"),
 				i,
 				strGuid,
 				sInfo[i].strInterfaceDescription,
-				GetInterfaceStateString(sInfo[i].isState));
+				GetInterfaceStateString(sInfo[i].isState),
+				GetInterfaceOperationModeString(OperationMode));
 
 			RpcStringFreeA(&strGuid);
 		}
@@ -203,8 +258,8 @@ int main()
 	UINT nSTate = 0;
 	//ULONG targetOperationMode = DOT11_OPERATION_MODE_EXTENSIBLE_STATION;
 	ULONG targetOperationMode = DOT11_OPERATION_MODE_NETWORK_MONITOR;
-	printf("Enter the state (0, 1 or 2) you want to switch to for the chosen wireless card:\n");
-	printf("0: Managed Mode (ExtSTA)\n1: Monitor Mode (NetMon)\n2: Master Mode (ExtAP)\n");
+	printf("Enter the operation mode (0, 1 or 2) you want to switch to for the chosen wireless card:\n");
+	printf("0: Extensible Station (ExtSTA)\n1: Network Monitor (NetMon)\n2: Extensible Access Point (ExtAP)\n");
 
 	if (ReadConsole(GetStdHandle(STD_INPUT_HANDLE), szBuffer, _countof(szBuffer), &dwRead, NULL) == FALSE)
 	{
